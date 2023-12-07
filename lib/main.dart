58c1ef90ef3f4774e57505eac50b6022f9db9eaa
shijin thottiyil <115061533+shijinthottiyil@users.noreply.github.com>
2023-12-07 12:09:32 +0530

    User interface -> Introdution -> Basic widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,76 @@
+///https://docs.flutter.dev/ui#basic-widgets <--Use this link to see the official documentaion.
+
 import 'package:flutter/material.dart';
+
+class MyAppBar extends StatelessWidget {
+  const MyAppBar({super.key, required this.title});
+
+  ///Fiels in a Widget subclass are always marked 'final'.
+  ///
+  final Widget title;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 56,
+      padding: const EdgeInsets.symmetric(horizontal: 8),
+      decoration: BoxDecoration(
+        color: Colors.blue[500],
+      ),
+      child: Row(
+        children: [
+          const IconButton(
+            icon: Icon(Icons.menu),
+            tooltip: 'Navigation menu',
+            onPressed: null,
+          ),
+          Expanded(
+            child: title,
+          ),
+          const IconButton(
+            onPressed: null,
+            icon: Icon(Icons.search),
+            tooltip: 'Search',
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class MyScaffold extends StatelessWidget {
+  const MyScaffold({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    ///Material is a conceptual piece of paper on which the UI appears.
+    ///
+    return Material(
+      child: Column(
+        children: [
+          MyAppBar(
+            title: Text(
+              'Example title',
+              style: Theme.of(context).primaryTextTheme.titleLarge,
+            ),
+          ),
+          const Expanded(
+            child: Center(
+              child: Text('Hello, world!'),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
 
 void main() {
   runApp(
-    const Center(
-      child: Text(
-        'Hello, world!',
-        textDirection: TextDirection.ltr,
+    const MaterialApp(
+      ///title is used by the OS task switcher.
+      title: 'My app',
+      home: SafeArea(
+        child: MyScaffold(),
       ),
     ),
   );
