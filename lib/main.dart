c3466c920db830f6b726ddf9def8dc808ccb0602
shijin thottiyil <115061533+shijinthottiyil@users.noreply.github.com>
2023-12-07 13:11:48 +0530

    User interface -> Introduction -> Using Material Components

--- lib/main.dart
+++ lib/main.dart
@@ -1,77 +1,46 @@
-///https://docs.flutter.dev/ui#basic-widgets <--Use this link to see the official documentaion.
+///https://docs.flutter.dev/ui#using-material-components
 
 import 'package:flutter/material.dart';
 
-class MyAppBar extends StatelessWidget {
-  const MyAppBar({super.key, required this.title});
+void main() {
+  runApp(
+    const MaterialApp(
+      title: 'Flutter Tutorial',
+      home: TutorialHome(),
+    ),
+  );
+}
 
-  ///Fiels in a Widget subclass are always marked 'final'.
-  ///
-  final Widget title;
+class TutorialHome extends StatelessWidget {
+  const TutorialHome({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 56,
-      padding: const EdgeInsets.symmetric(horizontal: 8),
-      decoration: BoxDecoration(
-        color: Colors.blue[500],
-      ),
-      child: Row(
-        children: [
-          const IconButton(
-            icon: Icon(Icons.menu),
-            tooltip: 'Navigation menu',
-            onPressed: null,
-          ),
-          Expanded(
-            child: title,
-          ),
-          const IconButton(
+    ///Scaffold is a layout for the major Material Components.
+    return Scaffold(
+      appBar: AppBar(
+        leading: const IconButton(
+          onPressed: null,
+          icon: Icon(Icons.menu),
+          tooltip: 'Navigation menu',
+        ),
+        title: const Text('Example title'),
+        actions: const [
+          IconButton(
             onPressed: null,
             icon: Icon(Icons.search),
             tooltip: 'Search',
           ),
         ],
       ),
-    );
-  }
-}
-
-class MyScaffold extends StatelessWidget {
-  const MyScaffold({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    ///Material is a conceptual piece of paper on which the UI appears.
-    ///
-    return Material(
-      child: Column(
-        children: [
-          MyAppBar(
-            title: Text(
-              'Example title',
-              style: Theme.of(context).primaryTextTheme.titleLarge,
-            ),
-          ),
-          const Expanded(
-            child: Center(
-              child: Text('Hello, world!'),
-            ),
-          ),
-        ],
+      body: const Center(
+        child: Text('Hello, world!'),
+      ),
+      floatingActionButton: const FloatingActionButton(
+        onPressed: null,
+        tooltip: 'Add',
+        child: Icon(Icons.add),
       ),
     );
   }
-}
-
-void main() {
-  runApp(
-    const MaterialApp(
-      ///title is used by the OS task switcher.
-      title: 'My app',
-      home: SafeArea(
-        child: MyScaffold(),
-      ),
-    ),
-  );
 }
