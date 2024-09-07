edc59da873583d0f5b5d261efe6b5a104d6ba3b2
shijinthottiyil <shijinthottiyil@gmail.com>
2024-09-07 11:49:28 +0530

    SCANNER APP PROTO FOR BLULINES

---
+++ lib/result_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ResultScreen extends StatelessWidget {
+  final String text;
+
+  const ResultScreen({super.key, required this.text});
+
+  @override
+  Widget build(BuildContext context) => Scaffold(
+        appBar: AppBar(
+          title: const Text('Result'),
+        ),
+        body: SingleChildScrollView(child: Text(text)),
+      );
+}
