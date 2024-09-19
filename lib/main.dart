a92b749aa540b91406eb22fa5f1a1cd1b9fcd357
shijinthottiyil <shijinthottiyil@gmail.com>
2024-09-19 15:53:31 +0530

    FLUTTER STAGGERED ANIMATIONS USING PACKAGE

--- lib/main.dart
+++ lib/main.dart
@@ -1,40 +1,80 @@
-/// https://docs.flutter.dev/ui#handling-gestures
-///
 import 'package:flutter/material.dart';
+import 'package:mylearning/widgets/elevated_layer_button.dart';
 
-class MyButton extends StatelessWidget {
-  const MyButton({super.key});
+import 'screens/card_column_screen.dart';
+import 'screens/card_grid_screen.dart';
+import 'screens/card_list_screen.dart';
+
+void main() => runApp(const App());
+
+class App extends StatelessWidget {
+  const App({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () {
-        print('MyButton was tapped!');
-      },
-      child: Container(
-        height: 50,
-        padding: const EdgeInsets.all(8),
-        margin: const EdgeInsets.symmetric(horizontal: 8),
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(5),
-          color: Colors.lightGreen[500],
-        ),
-        child: const Center(
-          child: Text('Engage'),
-        ),
+    return MaterialApp(
+      theme: ThemeData(
+        brightness: Brightness.light,
+        scaffoldBackgroundColor: const Color.fromRGBO(239, 238, 239, 1.0),
       ),
+      home: const HomeScreen(),
     );
   }
 }
 
-void main() {
-  runApp(
-    const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: MyButton(),
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SizedBox(
+        height: double.infinity,
+        width: double.infinity,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            ElevatedButton(
+              child: const Text('List Card Test'),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => const CardListScreen()),
+                );
+              },
+            ),
+            ElevatedButton(
+              child: const Text('Grid Card Test'),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => const CardGridScreen()),
+                );
+              },
+            ),
+            ElevatedButton(
+              child: const Text('Column Card Test'),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => const CardColumnScreen()),
+                );
+              },
+            ),
+            ElevatedLayerButton(
+              baseDecoration: const BoxDecoration(),
+              buttonHeight: kBottomNavigationBarHeight,
+              buttonWidth: double.maxFinite,
+              animationDuration: kThemeAnimationDuration,
+              animationCurve: Curves.bounceIn,
+              onClick: () {},
+            )
+          ],
         ),
       ),
-    ),
-  );
+    );
+  }
 }
---
+++ lib/widgets/elevated_layer_button.dart
@@ -0,0 +1,129 @@
+import 'package:flutter/material.dart';
+
+///
+/// [ElevatedLayerButton] Renders Button layers in 3D perspective
+///
+class ElevatedLayerButton extends StatefulWidget {
+  /// Define height for button
+  final double? buttonHeight;
+
+  /// Define width for button
+  final double? buttonWidth;
+
+  /// Define Clicking Duration - [Duration(milliseconds: 200)]
+  final Duration? animationDuration;
+
+  /// Define Animation Curve - [Curves.ease]
+  final Curve? animationCurve;
+
+  /// Void Function for Button click
+  final VoidCallback? onClick;
+
+  /// Define Base Layer [BoxDecoration] for button
+  final BoxDecoration? baseDecoration;
+
+  /// Define Top Layer [BoxDecoration] for button
+  final BoxDecoration? topDecoration;
+
+  /// Define Top Layer Child [Widget] for button
+  final Widget? topLayerChild;
+
+  /// if anyone wants to add slightly rounded corners
+  final BorderRadius? borderRadius;
+
+  const ElevatedLayerButton({
+    Key? key,
+    required this.buttonHeight,
+    required this.buttonWidth,
+    required this.animationDuration,
+    required this.animationCurve,
+    required this.onClick,
+    this.baseDecoration,
+    this.topDecoration,
+    this.topLayerChild,
+    this.borderRadius,
+  }) : super(key: key);
+
+  @override
+  State<ElevatedLayerButton> createState() => _ElevatedLayerButtonState();
+}
+
+class _ElevatedLayerButtonState extends State<ElevatedLayerButton> {
+  bool buttonPressed = false;
+
+  bool animationCompleted = true;
+
+  bool get _enabled => widget.onClick != null;
+
+  bool get _disabled => !_enabled;
+
+  @override
+  Widget build(BuildContext context) {
+    return Opacity(
+      opacity: _disabled ? 0.5 : 1,
+      child: GestureDetector(
+        onTap: () {
+          if (!_disabled) {
+            setState(() {
+              buttonPressed = true;
+              animationCompleted = false;
+            });
+          }
+        },
+        child: SizedBox(
+          height: widget.buttonHeight,
+          width: widget.buttonWidth,
+          child: Stack(
+            alignment: Alignment.bottomRight,
+            children: [
+              Positioned(
+                bottom: 0,
+                right: 0,
+                child: Container(
+                  width: (widget.buttonWidth ?? 100) - 10,
+                  height: (widget.buttonHeight ?? 40) - 10,
+                  decoration: widget.baseDecoration?.copyWith(
+                        borderRadius: widget.borderRadius,
+                      ) ??
+                      BoxDecoration(
+                        color: Colors.black,
+                        border: Border.all(color: Colors.black),
+                      ),
+                ),
+              ),
+              AnimatedPositioned(
+                bottom: _enabled ? (buttonPressed ? 0 : 4) : 4,
+                right: _enabled ? (buttonPressed ? 0 : 4) : 4,
+                duration: widget.animationDuration ??
+                    const Duration(milliseconds: 300),
+                curve: widget.animationCurve ?? Curves.ease,
+                onEnd: () {
+                  if (!animationCompleted) {
+                    animationCompleted = true;
+                    setState(() => buttonPressed = false);
+                    if (_enabled) {
+                      widget.onClick!();
+                    }
+                  }
+                },
+                child: Container(
+                  width: (widget.buttonWidth ?? 100) - 10,
+                  height: (widget.buttonHeight ?? 100) - 10,
+                  alignment: Alignment.center,
+                  decoration: widget.topDecoration?.copyWith(
+                        borderRadius: widget.borderRadius,
+                      ) ??
+                      BoxDecoration(
+                        color: Colors.black,
+                        border: Border.all(color: Colors.black),
+                      ),
+                  child: widget.topLayerChild,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
