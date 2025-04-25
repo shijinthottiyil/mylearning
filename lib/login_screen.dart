7fd56f541d7e3cae3b1bf90c47844f78db8f5d33
shijinthottiyil <shijinthottiyil@gmail.com>
2025-04-25 18:51:17 +0530

    BASITH DONE LOGIN

--- lib/login_screen.dart
+++ lib/login_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:mylearning/home_screen.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
@@ -9,109 +8,62 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
-  // VARIABLES--------------------------------------
-  late TextEditingController _userNameEditingController;
-  late TextEditingController _passwordEditingController;
-  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
-  final _userName = 'basith';
-  final _password = '123456';
-
-  // METHODS----------------------------------------
-  @override
-  void initState() {
-    super.initState();
-    _init();
-  }
-
-  void _init() {
-    _userNameEditingController = TextEditingController();
-    _passwordEditingController = TextEditingController();
-  }
-
-  @override
-  void dispose() {
-    _dispose();
-    super.dispose();
-  }
-
-  void _dispose() {
-    _userNameEditingController.dispose();
-    _passwordEditingController.dispose();
-  }
-
-  void _onLogin() {
-    if (_formKey.currentState!.validate()) {
-      Navigator.pushReplacement(
-          context, MaterialPageRoute(builder: (context) => const HomeScreen()));
-    }
-  }
+  final _formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Login Screen'),
+        title: const Text('Basith'),
       ),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 16),
-        child: Center(
-          child: Form(
-            key: _formKey,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              mainAxisSize: MainAxisSize.max,
-              children: [
-                TextFormField(
-                  controller: _userNameEditingController,
-                  decoration: const InputDecoration(
-                    labelText: 'Username',
-                    border: OutlineInputBorder(),
-                  ),
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Please enter your username';
-                    }
-                    if (value != _userName) {
-                      return 'Invalid username';
-                    }
-                    return null;
-                  },
-                ),
-                const SizedBox(height: 16),
-                TextFormField(
-                  controller: _passwordEditingController,
-                  decoration: const InputDecoration(
-                    labelText: 'Password',
-                    border: OutlineInputBorder(),
-                  ),
-                  obscureText: true,
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Please enter your password';
-                    }
-                    if (value != _password) {
-                      return 'Invalid password';
-                    }
-                    return null;
-                  },
-                ),
-                const SizedBox(height: 16),
-                GestureDetector(
-                  onTap: _onLogin,
-                  child: Container(
-                    width: double.maxFinite,
-                    height: kBottomNavigationBarHeight,
-                    decoration: const BoxDecoration(
-                      color: Colors.blue,
-                    ),
-                    child: const Center(
-                      child: Text('Login'),
-                    ),
-                  ),
-                ),
-              ],
+      body: Form(
+        key: _formKey,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextFormField(
+              decoration: const InputDecoration(
+                hintText: 'Enter username',
+              ),
+              // The validator receives the text that the user has entered.
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Please enter username';
+                }
+                return null;
+              },
             ),
-          ),
+            TextFormField(
+              obscureText: true,
+              obscuringCharacter: '*',
+              decoration: const InputDecoration(
+                hintText: 'Enter Password',
+              ),
+              // The validator receives the text that the user has entered.
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Please enter password';
+                }
+                if (value.length < 8) {
+                  return 'Password must be at least 8 characters';
+                }
+                return null;
+              },
+            ),
+            ElevatedButton(
+              onPressed: () {
+                // Validate returns true if the form is valid, or false otherwise.
+                if (_formKey.currentState!.validate()) {
+                  // If the form is valid, display a snackbar. In the real world,
+                  // you'd often call a server or save the information in a database.
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    const SnackBar(content: Text('Processing Data')),
+                  );
+                }
+              },
+              child: const Text('Login'),
+            ),
+          ],
         ),
       ),
     );
